6d1af82949fb5fb1cf62bc12704c788104eda757
Kristina Andrijin <andrijinkristina@gmail.com>
2022-12-07 15:18:37 +0100

    Fixed close button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  display = false;
+  onPress() {
+    this.display = true;
+  }
 
-}
+   /*if you want the component to show and hide on click pressed, use 
+   use this line
+   this.display = !this.display;*/
+ }

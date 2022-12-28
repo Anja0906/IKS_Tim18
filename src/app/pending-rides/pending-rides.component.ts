e042156813b5fc3e1241daff3c5431622380c40c
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2022-12-28 21:42:11 +0100

    Driver component
    4.2 about ride
    4.1 map prototype
    4.4 Drivers profile prototype
    4.6 panic button design

---
+++ src/app/pending-rides/pending-rides.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-rides',
+  templateUrl: './pending-rides.component.html',
+  styleUrls: ['./pending-rides.component.css']
+})
+export class PendingRidesComponent {
+
+}

bfb999091e212d4ed6e76aa13e82e315774cbc65
Kristina Andrijin <andrijinkristina@gmail.com>
2023-01-19 16:17:16 +0100

    Added interceptor

--- src/app/interceptor.ts
+++ src/app/interceptor.ts
@@ -2,14 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class HttpRequestInterceptor implements HttpInterceptor {
+  constructor() {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    req = req.clone({
-     //withCredentials: true,
-    });
-
-    return next.handle(req);
+    const jwt = localStorage.getItem('jwt');
+    console.log(jwt);
+    if (jwt) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization : 'Bearer ${jwt}'
+        }
+      });
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
+    }
   }
 }
 

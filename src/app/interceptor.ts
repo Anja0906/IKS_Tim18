b9d0711d7e89cf59d1d2e8264235be97fef49cc7
Kristina Andrijin <andrijinkristina@gmail.com>
2023-01-19 17:27:03 +0100

    Fix jwt

--- src/app/interceptor.ts
+++ src/app/interceptor.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { StorageService } from './service/storage/storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpRequestInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private storageService: StorageService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const jwt = localStorage.getItem('jwt');
     console.log(jwt);
-    if (jwt) {
+    console.log(this.storageService.getUser().id);
+    const jwtSpring = this.storageService.getUser().accessToken;
+    console.log(jwtSpring);
+    if (jwtSpring) {
       const cloned = req.clone({
         setHeaders: {
-          Authorization : 'Bearer ${jwt}'
+          Authorization: `Bearer ${jwtSpring}`
         }
       });
       return next.handle(cloned);

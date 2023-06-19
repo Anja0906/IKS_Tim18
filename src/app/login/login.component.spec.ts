2560fa2df6d8340ba44c1f14aa417a91639f25b4
Anja Petković <anjapetkovic92@gmail.com>
2023-06-19 02:08:59 +0200

    Login angular tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,23 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
+      declarations: [ LoginComponent ],
+      providers: [ FormBuilder ],
     })
-    .compileComponents();
+  });
 
+  beforeEach(() =>{
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('form should valid', () =>{
+    component.loginForm.controls['email'].setValue('anita@gmail.com');
+    component.loginForm.controls['password'].setValue('123');
+    expect(component.loginForm.valid).toBeTruthy();
+  });
+
+  it('form should be invalid when missing input', () =>{
+    component.loginForm.controls['email'].setValue('anita@gmail.com');
+    component.loginForm.controls['password'].setValue('');
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('form should be invalid when no input', () =>{
+    component.loginForm.controls['email'].setValue('');
+    component.loginForm.controls['password'].setValue('');
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
 });
+
